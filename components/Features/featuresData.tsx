10cb7e355101b5d325faa2c47c67644694567177
ginopastran <ginopastran@gmail.com>
2025-01-24 11:13:55 -0300

    first commit

---
+++ components/Features/featuresData.tsx
@@ -0,0 +1,48 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/icon/icon-01.svg",
+    title: "Desarrollo Web",
+    description:
+      "Creamos páginas web a medida, elegantes y funcionales, adaptadas a las necesidades de tu negocio.",
+  },
+  {
+    id: 2,
+    icon: "/images/icon/icon-02.svg",
+    title: "Sistemas a Medida",
+    description:
+      "Desarrollamos sistemas personalizados que optimizan procesos y mejoran la eficiencia de tu empresa.",
+  },
+  {
+    id: 3,
+    icon: "/images/icon/icon-03.svg",
+    title: "Asesorías",
+    description:
+      "Ofrecemos asesorías y auditorías en software para garantizar el mejor rendimiento y calidad.",
+  },
+  // {
+  //   id: 4,
+  //   icon: "/images/icon/icon-04.svg",
+  //   title: "Sanity Blog and Docs",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  // },
+  // {
+  //   id: 5,
+  //   icon: "/images/icon/icon-05.svg",
+  //   title: "DB, Auth and Stripe",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  // },
+  // {
+  //   id: 6,
+  //   icon: "/images/icon/icon-06.svg",
+  //   title: "Regular Free Updates",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  // },
+];
+
+export default featuresData;
